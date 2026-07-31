fa8b0066075885320dbccb340a97cdf8db8fb44e
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-31 12:27:33 +0300

    feat: add journal entry

--- models/journal-entry.py
+++ models/journal-entry.py
@@ -4,3 +4,8 @@ from . import db
 
 class JournalEntry(db.Model):
     __tablename__ = "journal_entries"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    
