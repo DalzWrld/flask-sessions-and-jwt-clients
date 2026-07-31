8f2a73db80163a91b78c55f4802ac053d4c1a29b
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-31 13:11:25 +0300

    feat: add updated project setup

--- resources/auth.py
+++ resources/auth.py
@@ -1,50 +1,47 @@
-from flask import request, make_response
+from flask import make_response, request, session
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 from flask_restful import Resource
+from marshmallow import IntegrityError, ValidationError
 
-from models import User, db
-from schemas import user_schema, register_schema
+from extensions import log
+from models.user import User, db
+from schemas import register_schema, user_schema
 
 
 class Register(Resource):
     def post(self):
         try:
             data = request.get_json()
+            validated_data = register_schema.load(data=data)
             
-                validated_data = register_schema.load(data=data)
-            
-                if User.query.filter_by(
-                        email_address=validated_data["email_address"]
-                    ).first():
-                        return make_response(
-                            {"status": 409, "message": "Email address already taken"}, 409
-                        )
-                if User.query.filter_by(phone=validated_data["phone"]).first():
-                    return make_response(
-                        {"status": 409, "message": "Phone number already taken"}, 409
-                    )
-                    
-                user = User(
-                    first_name=validated_data["first_name"],
-                    last_name=validated_data["last_name"],
-                    email_address=validated_data["email_address"],
-                    phone=validated_data["phone"],
+            if User.query.filter_by(email_address=validated_data["email_address"]).first():
+                return make_response(
+                    {"status": 409, "message": "Email address already taken"}, 409
                 )
-            
+            if User.query.filter_by(phone=validated_data["phone"]).first():
+                return make_response(
+                    {"status": 409, "message": "Phone number already taken"}, 409
+                )
                     
-                user.set_password(validated_data["password"])
-            
-                db.session.add(user)
-                db.session.commit()
-            
+            user = User(
+                first_name=validated_data["first_name"],
+                last_name=validated_data["last_name"],
+                email_address=validated_data["email_address"],
+                phone=validated_data["phone"],
+            )
                     
-                session["user_id"] = user.id
+            user.set_password(validated_data["password"])
             
-                response = {
-                    "message": "Account created successfully",
-                    "data": user_schema.dump(user)
-                }
-                return make_response(response, 200)
+            db.session.add(user)
+            db.session.commit()
+            
+            session["user_id"] = user.id
+            
+            response = {
+                "message": "Account created successfully",
+                "data": user_schema.dump(user)
+            }
+            return make_response(response, 200)
 
         except ValidationError as err:
             log.error("validation_error", errors=err.messages)
