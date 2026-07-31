e8c703431fddcfd40444df31ff85c8957b8852e0
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-31 12:40:22 +0300

    feat: add journal schema

--- schemas/journal-schema.py
+++ schemas/journal-schema.py
@@ -2,4 +2,9 @@ from marshmallow import Schema, fields
 
 
 class JournalEntrySchema(Schema):
-    
+    id = fields.Int(dump_only=True)
+    title = fields.Str(required=True)
+    content = fields.Str(required=True)
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+    user_id = fields.Int(dump_only=True)
