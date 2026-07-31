2728a9efacc977eefabd3f142b4109ffd1bc768a
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-31 14:58:07 +0300

    feat: add seed

---
+++ seed.py
@@ -0,0 +1,46 @@
+from faker import Faker
+import random
+
+from app import app
+from models import db, User, JournalEntry
+
+fake = Faker()
+
+
+with app.app_context():
+
+    db.drop_all()
+    db.create_all()
+
+    users = []
+
+    for _ in range(10):
+
+        user = User(
+            username=fake.user_name(),
+            email=fake.unique.email()
+        )
+
+        user.password = "password123"
+
+        users.append(user)
+
+    db.session.add_all(users)
+    db.session.commit()
+
+    entries = []
+
+    for _ in range(50):
+
+        entry = JournalEntry(
+            title=fake.sentence(nb_words=5),
+            content=fake.paragraph(nb_sentences=5),
+            user_id=random.choice(users).id
+        )
+
+        entries.append(entry)
+
+    db.session.add_all(entries)
+    db.session.commit()
+
+    print("Database seeded successfully!")
