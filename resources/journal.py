eca80678791feb7a388eca96ee1b56a5beb73272
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-31 14:05:38 +0300

    feat: add journal

--- resources/journal.py
+++ resources/journal.py
@@ -10,3 +10,19 @@ from schemas import (
     journal_schema,
     journals_schema
 )
+
+class JournalList(Resource):
+    @jwt_required()
+    def get(self):
+
+        current_user = get_jwt_identity()
+
+        page = request.args.get("page", 1, type=int)
+        per_page = request.args.get("per_page", 5, type=int)
+
+        pagination = (
+            JournalEntry.query
+            .filter_by(user_id=current_user)
+            .order_by(JournalEntry.created_at.desc())
+            .paginate(page=page, per_page=per_page)
+        )
