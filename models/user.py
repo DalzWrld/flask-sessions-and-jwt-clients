6b053b3893de9e0b2db3ae2b1d62c84330587f81
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-31 12:22:35 +0300

    feat: add user && config

--- models/user.py
+++ models/user.py
@@ -1,18 +1,7 @@
 from flask_bcrypt import check_password_hash, generate_password_hash
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 
-metadata = MetaData(
-    naming_convention={
-        "ix": "ix_%(column_0_label)s",
-        "uq": "uq_%(table_name)s_%(column_0_name)s",
-        "ck": "ck_%(table_name)s_%(constraint_name)s",
-        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-        "pk": "pk_%(table_name)s",
-    }
-)
+from . import db
 
-db = SQLAlchemy(metadata=metadata)
 
 class User(db.Model):
     __tablename__ = "users"
