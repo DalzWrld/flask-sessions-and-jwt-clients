5299105e4d06117e692a1f0a741fe5f2951853f2
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-31 14:41:06 +0300

    feat: add updated project setup

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+
+from dotenv import load_dotenv
+from flask import Flask
+from flask_migrate import Migrate
+from flask_restful import Api
+
+from config import Config
+from extensions import bcrypt, jwt
+from models import db
+from resources.auth import LoggedIn, Login, Logout, Register
+from resources.journal import Journal, JournalList
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URI",
+    "sqlite:///instance/app.db"
+)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+app.config["SECRET_KEY"] = os.getenv(
+    "SECRET_KEY"
+)
+
+app.config["JWT_SECRET_KEY"] = os.getenv(
+    "JWT_SECRET_KEY"
+)
+
+db.init_app(app)
+bcrypt.init_app(app)
+jwt.init_app(app)
+
+migrate = Migrate(app, db)
+api = Api(app)
+
+
+api.add_resource(Register, "/register")
+api.add_resource(Login, "/login")
+api.add_resource(Logout, "/logout")
+api.add_resource(LoggedIn, "/loggedin")
+
+api.add_resource(JournalList, "/journal")
+api.add_resource(Journal, "/journal/<int:id>")
+
+
+@app.route("/")
+def home():
+    return {
+        "message": "Journal API is running!"
+    }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
