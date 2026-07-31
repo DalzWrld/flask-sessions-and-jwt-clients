3be9ddfb86035e8fac50687c1c812a934922e713
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-31 12:17:57 +0300

    feat: add user

--- models/user.py
+++ models/user.py
@@ -19,4 +19,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    journal_entries = db.relationship("JournalEntry", back_populates="user", cascade="all, delete-orphan", lazy=True)
